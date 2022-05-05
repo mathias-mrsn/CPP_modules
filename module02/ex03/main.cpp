527f89015ec06a3d70edc76c5e4be91ae01c8cb6
Mathias MAURAISIN <mathias.mrsn@gmail.com>
2022-05-05 09:46:51 +0200

    module02->ex03 fixed, module is ready to be pushed

--- module02/ex03/main.cpp
+++ module02/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mamaurai <mamaurai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/27 10:26:40 by mamaurai          #+#    #+#             */
-/*   Updated: 2022/05/02 14:21:53 by mamaurai         ###   ########.fr       */
+/*   Updated: 2022/05/05 09:44:26 by mamaurai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,21 @@
 int	main(void) {
 	
 	Point a(0, 0);
-	Point b(5, 15);
+	Point b(0, 15);
 	Point c(18, 7);
 
-	Point* tests = new Point[5];
+	Point* tests = new Point[8];
 
 	tests[0] = Point(0, 0);
 	tests[1] = Point(10, 6);
 	tests[2] = Point(4, 18);
 	tests[3] = Point(9, 11);
 	tests[4] = Point(0, 1);
+	tests[5] = Point(0.001, 1);
+	tests[6] = Point(2, 5);
+	tests[7] = Point(18, 7);
 	
-	for (int i = 0; i < 5; i++)
+	for (int i = 0; i < 8; i++)
 	{
 		std::cout << "Point x = " << tests[i].getXFloat() << ", y = " << tests[i].getYFloat() << " "; 
 		if (true == bsp(a, b, c, tests[i]))
@@ -41,29 +44,3 @@ int	main(void) {
 
 	return 0;
 }
-
-/*
-
-xxxxxxxxxxxxxxxxxxxx
-xxxxxxxxxxxxxxxxxxxx
-xxxx2xxxxxxxxxxxxxxx
-xxxxxxxxxxxxxxxxxxxx
-xxxxxxxxxxxxxxxxxxxx
-xxxxxbxxxxxxxxxxxxxx
-xxxxxxxxxxxxxxxxxxxx
-xxxxxxxxxxxxxxxxxxxx
-xxxxxxxxxxxxxxxxxxxx
-xxxxxxxxx3xxxxxxxxxx
-xxxxxxxxxxxxxxxxxxxx
-xxxxxxxxxxxxxxxxxxxx
-xxxxxxxxxxxxxxxxxxxx
-xxxxxxxxxxxxxxxxxxxx
-xxxxxxxxxx1xxxxxxxxx
-xxx0xxxxxxxxxxxxxxxx
-xxxxxxxxxxxxxxxxxxxx
-xxxxxxxxxxxxxxxxxxxx
-xxxxxxxxxxxxxxxxxxxx
-x4xxxxxxxxxxxxxxxxxx
-axxxxxxxxxxxxxxxxxcx
-
-*/
