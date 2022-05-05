6bf8f67682f3dc0c78a5fe06287b9f28bd41287d
Mathias MAURAISIN <mamaurai@e3r11p15.clusters.42paris.fr>
2022-05-05 18:20:10 +0200

    module00->ex01 in progress

--- module05/ex00/main.cpp
+++ module05/ex00/main.cpp
@@ -6,9 +6,72 @@
 /*   By: mamaurai <mamaurai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/04 14:24:28 by mamaurai          #+#    #+#             */
-/*   Updated: 2022/05/04 14:24:36 by mamaurai         ###   ########.fr       */
+/*   Updated: 2022/05/05 18:08:13 by mamaurai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
+int
+main (int ac, char **av)
+{
+	try
+	{
+		Bureaucrat		paul = Bureaucrat("Paul", 1);
+		std::cout << paul << std::endl;
+		paul.lowerRank(10);
+		std::cout << paul << std::endl;
+		paul.upperRank(10);
+		std::cout << paul << std::endl;
+		paul.upperRank(10);
+		std::cout << paul << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	std::cout << std::endl;
+	
+	
+	// try
+	// {
+	// 	Bureaucrat		paul = Bureaucrat("Paul", 150);
+	// 	std::cout << paul << std::endl;
+	// 	paul.upperRank(10);
+	// 	std::cout << paul << std::endl;
+	// 	paul.lowerRank(10);
+	// 	std::cout << paul << std::endl;
+	// 	paul.lowerRank(10);
+	// }
+	// catch (std::exception &e)
+	// {
+	// 	std::cout << e.what() << std::endl;
+	// }
+	// std::cout << std::endl;
+	
+	
+	// try
+	// {
+	// 	Bureaucrat		paul = Bureaucrat("Paul", 0);
+	// 	std::cout << paul << std::endl;
+	// 	paul.lowerRank(10);
+	// }
+	// catch (std::exception &e)
+	// {
+	// 	std::cout << e.what() << std::endl;
+	// }
+	// std::cout << std::endl;
+	
+	
+	// try
+	// {
+	// 	Bureaucrat		paul = Bureaucrat("Paul", 151);
+	// 	std::cout << paul << std::endl;
+	// 	paul.upperRank(10);
+	// }
+	// catch (std::exception &e)
+	// {
+	// 	std::cout << e.what() << std::endl;
+	// }
+	
+}
