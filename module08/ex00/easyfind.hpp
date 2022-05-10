0594b29fa7101d7682610b49d6dd2f8e32ac0c64
Mathias MAURAISIN <mathias.mrsn@gmail.com>
2022-05-10 17:54:08 +0200

    module08->ex00 is done

---
+++ module08/ex00/easyfind.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mamaurai <mamaurai@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/10 17:40:27 by mamaurai          #+#    #+#             */
+/*   Updated: 2022/05/10 17:53:31 by mamaurai         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+class IsNotInContainer : public std::exception
+{
+	public:
+		virtual const char * what (void) const throw() {return ("Impossible to find this value in the container");}
+};
+
+template<typename T>
+bool
+easyfind (T container, int toFind)
+{
+	if (std::find(container.begin(), container.end(), toFind) != container.end())
+		return (true);
+	else
+		throw IsNotInContainer();
+}
---
+++ module08/ex00/main.cpp
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mamaurai <mamaurai@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/10 17:48:03 by mamaurai          #+#    #+#             */
+/*   Updated: 2022/05/10 17:53:19 by mamaurai         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "easyfind.hpp"
+
+int
+main (__attribute__((unused)) int ac, __attribute__((unused)) char **av)
+{
+	std::vector<int> lst;
+
+	lst.push_back(42);
+	lst.push_back(2);
+	lst.push_back(521);
+	lst.push_back(12);
+	lst.push_back(982);
+	lst.push_back(1242);
+
+	try
+	{
+		easyfind(lst, 521);
+		std::cout << "The value has been found !" << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	
+	try
+	{
+		easyfind(lst, 0);
+		std::cout << "The value has been found !" << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
+		try
+	{
+		easyfind(lst, 12);
+		std::cout << "The value has been found !" << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+}
